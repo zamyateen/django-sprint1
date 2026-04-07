c2870a1e91e678649cffe00444b2af3401874c17
Alexey <popov300693@gmail.com>
2026-04-07 22:15:58 +0300

    Исправлен текст постов

--- blog/views.py
+++ blog/views.py
@@ -54,4 +54,8 @@ def post_detail(request, id):
 
 
 def category_posts(request, category_slug):
-    return render(request, 'blog/category.html', {'category_slug': category_slug})
+    return render(
+        request,
+        'blog/category.html',
+        {'category_slug': category_slug}
+    )
